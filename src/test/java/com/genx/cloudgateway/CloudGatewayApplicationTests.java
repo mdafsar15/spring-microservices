72db26b267bd315dc7dc52d4251c857c222e2423
afsar.ansari <afsaralam7200@gmail.com>
2022-03-11 15:02:17 +0530

    cloud gateway

---
+++ src/test/java/com/genx/cloudgateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,11 @@
+package com.genx.cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	
+
+}
